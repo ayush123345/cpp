46a5056917faf89292748fd376f80ec951869b66
ayush123345 <ayushzodape0@gmail.com>
2024-02-28 19:01:45 +0530

    n

--- initial/Queue.c
+++ initial/Queue.c
@@ -1,3 +1,25 @@
-//
-// Created by HP on 28-02-2024.
-//
+#include <stdio.h>
+#define N 5
+int queue [N] ;
+int front = -1 ;
+int rear = -1 ;
+
+void enqueue () {
+    int x;
+    printf("Enter the value which is inserted in a queue");
+    scanf("%d", &x);
+
+    if (rear == N - 1) {
+        printf("Queue is full");
+    } else if (front == -1&& rear ==-1)
+    {
+        front=rear=0;
+        queue[rear]=x;
+    }else{
+        rear++;
+        queue[rear]=x;
+    }
+}
+int main(){
+    enqueue();
+}
